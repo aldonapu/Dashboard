df4a34260ce0394676550251579a0bb1cfb7a5ee
aldonapu <aldonapu10@gmail.com>
2026-07-12 18:13:04 +0800

    update

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-console.log(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
